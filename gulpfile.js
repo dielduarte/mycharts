7a4aeb1483eb54bda41e8396f16b86d0bb25dfed
Magdiel <magdiel.kesller@gmail.com>
2014-11-20 01:01:06 -0200

    new dependencies, files and folders and config gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp  = require('gulp'),
 rename    = require('gulp-rename'),
-htmlmin   = require('gulp-htmlmin');
+htmlmin   = require('gulp-htmlmin'),
+minifyCSS = require('gulp-minify-css'),
+concat    = require('gulp-concat');
 
 
 
 var pathsDev = {
-  html: ['./dev/*.html']
+  html: ['./dev/*.html'],
+  styles: ['./dev/assets/css/*.css'],
 };
 
 
@@ -16,13 +19,23 @@ gulp.task('minifyhtml', function() {
     .pipe(gulp.dest('./build/'));
 });
 
+// Task for concat and minfier  and convert sass to css files 
+gulp.task('concat-min-css', function() {
+  gulp.src(pathsDev.styles)
+    .pipe(concat('main.css'))
+    .pipe(minifyCSS())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./build/assets/css/'));
+});
+
 
 gulp.task('watch',function(){
     gulp.watch(pathsDev.html,  ['minifyhtml']);
+    gulp.watch(pathsDev.styles, ['concat-min-css']);
 });
 
 // Taks default gulp! 
-gulp.task('default', ['minifyhtml', 'watch'], function(){
+gulp.task('default', ['minifyhtml', 'concat-min-css', 'watch'], function(){
 	console.log('gulp ok!');
 });
 
